54791cffd3e8b307af14aef2274acf614a9df491
pray-lee <praycis@gmail.com>
2018-09-25 09:51:08 +0800

    update geojson

--- src/api/mapStyle/layers.js
+++ src/api/mapStyle/layers.js
@@ -3,7 +3,7 @@ const layers = [
     "id": "water",
     "type": "fill",
     "source": "geojson-lines",
-    "source-layer": "water",
+    // "source-layer": "water",
     "minzoom": 0,
     "maxzoom":22,
     "paint": {
